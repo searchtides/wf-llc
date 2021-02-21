8a16c71a7579e72c5ede673021335a7eec1c426f
yyk <yyk@mail.ru>
2021-02-21 09:59:42 -0400

    fetch.om refactored

--- fetch.js
+++ fetch.js
@@ -23,28 +23,12 @@ fetch.airtable = function(a) {
 
 };
 
-fetch.om2 = function(offset) {
+fetch.om = function(offset) {
   var table_name, fields, formula;
   table_name = 'OM';
   fields = ['STATUS 1', 'Import Date'].concat(FIELDS);
   formula = "{STATUS 1} = 'Published'";
   return fetch.airtable({table_name : table_name, fields : fields, formula : formula, offset : offset});
-};
-
-//::IO()->Either Error Hashmap
-fetch.om = function(offset) {
-  var url, table_name, options, res, h, result_map, fn, field, formula, fields, fields_str;
-  //formula = "AND(LEFT({Import Date}, 4) = '2021', {STATUS 1} = 'Published')";
-  formula = "{STATUS 1} = 'Published'";
-  table_name = 'OM';
-  fields = ['STATUS 1', 'Import Date'].concat(FIELDS);
-  fields_str = fields.map(function(field) {return encodeURI('fields[]=' + field);}).join('&');
-  url = [AIRTALBE_ENDPOINT, DATABASE_ID, table_name].join('/') + '?' + fields_str + '&filterByFormula=' + encodeURI(formula);
-  if (offset) {url += '&offset=' + offset;}
-  options =  {method : 'get', headers : HEADERS, muteHttpExceptions : true};
-  fn = function(s) {return JSON.parse(s);};
-  res = http_req({url : url, options : options, fn : fn});
-  return res;
 };
 
 //:: IO () -> Either Error Hashmap
