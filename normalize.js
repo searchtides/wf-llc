9002401384d77f195d3e1b5379bbc14d67c71559
yyk <yyk@mail.ru>
2021-03-16 22:11:11 -0400

    normalize module with archive normalizing added

--- normalize.js
+++ normalize.js
@@ -2,6 +2,7 @@ var normalize = {};
 
 normalize.archive = function(archive) {
   return archive.map(function(ar) {
+    ar['IP Location'] = ar['IP Location'].trim();
     if (ar['Follow/NoFollow'] == 'F') {ar['Follow/NoFollow'] = 'Do-Follow';}
     if (ar['Follow/NoFollow'] == 'NF') {ar['Follow/NoFollow'] = 'No-Follow';}
     var month;
---
+++ normalize_tests.js
@@ -0,0 +1,17 @@
+function normalize_module_tests() {
+  return;
+}
+
+function test_normalize_archive() {
+  return jUnit.test_case('', {
+    'test normalizing archive' : function() {
+      var archive, res;
+      archive = ssa.get_vh(tt.ds('0.6'));
+      jUnit.assert_eq_num(521, archive.length);
+      res = normalize.archive(archive);
+      jUnit.assert_eq_num(521, res.length);
+      ssa.put_vh(tt.tb('0.1'), res);
+
+    }
+  });
+}
