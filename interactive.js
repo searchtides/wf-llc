4128b1aa9f1688f53113b6a59f736cdbd42d7a39
yyk <yyk@mail.ru>
2021-05-18 14:33:57 -0400

    fix: daily trigger installation fixed

--- interactive.js
+++ interactive.js
@@ -63,7 +63,7 @@ function install_triggers() {
   var ss;
   remove_triggers();
   ss = SpreadsheetApp.getActive();
-  ScriptApp.newTrigger(TIME_DAEMON).timeBased().everyMinutes(1).create();
+  ScriptApp.newTrigger('daily').timeBased().everyMinutes(1).create();
   Browser.msgBox('All triggers have been installed under ' + Session.getActiveUser() + ' account');
 }
 
